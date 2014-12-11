74caaf8c25a0b3888b9e82ac23e0c366b4eb9169
Adams Tower <herpestesfantasticus@gmail.com>
2014-12-11 11:21:23 -0500

    Display user in fish powerline

--- config.fish
+++ config.fish
@@ -6,6 +6,7 @@ set fish_path $HOME/.oh-my-fish
 set fish_theme bobthefish
 
 set -g theme_display_ruby no
+set -g theme_display_user yes
 
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
 # Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
