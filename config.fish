fe34628140d54956f03deeb6965ddc21b037311b
Adams Tower <herpestesfantasticus@gmail.com>
2014-12-11 12:21:02 -0500

    Adds some highlighting colors

--- config.fish
+++ config.fish
@@ -8,6 +8,15 @@ set fish_theme bobthefish
 set -g theme_display_ruby no
 set -g theme_display_user yes
 
+# Highlighting colors
+set -U fish_color_command    00875f
+set -U fish_color_error      d70000
+set -U fish_color_match      00d7af
+set -U fish_color_operator   00d7af
+set -U fish_color_param      00d7af
+set -U fish_color_quote      878700
+set -U fish_color_valid_path 00d7af\x1e\x2d\x2dunderline
+
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
 # Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
 # Example format: set fish_plugins autojump bundler
